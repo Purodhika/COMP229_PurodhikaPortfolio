948ebd52c22e42a9ca3e280d492c14e8e28312c8
Purodhika <purodhika.s@gmail.com>
2022-06-08 12:51:23 -0400

    express portfolio commit

---
+++ public/scripts/app.js
@@ -0,0 +1,8 @@
+(function()
+{
+function Start()
+{
+    console.log("App started...");
+}
+window.addEventListener("Load", Start());
+})();
---
+++ routes/users.js
@@ -0,0 +1,16 @@
+/*
+File: users.js
+Name: Purodhika Sharma
+Student ID: 301223212
+Date: 03-06-22
+*/
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
